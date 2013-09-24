064c85f4a7b4cb9eb0d8c94b06a8d97b530fc0c7
Fernando Israél García Martínez <frekofefe@gmail.com>
2013-09-24 13:33:27 +0200

    Info to create databases.

--- createdatabase.sh
+++ createdatabase.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
-#
-# mysql -u root -p
-
-# #Keystone
-# CREATE DATABASE keystone;
-# GRANT ALL ON keystone.* TO 'keystoneUser'@'%' IDENTIFIED BY 'keystonePass';
-# 
-# #Glance
-# CREATE DATABASE glance;
-# GRANT ALL ON glance.* TO 'glanceUser'@'%' IDENTIFIED BY 'glancePass';
-# 
-# #Quantum
-# CREATE DATABASE quantum;
-# GRANT ALL ON quantum.* TO 'quantumUser'@'%' IDENTIFIED BY 'quantumPass';
-# 
-# #Nova
-# CREATE DATABASE nova;
-# GRANT ALL ON nova.* TO 'novaUser'@'%' IDENTIFIED BY 'novaPass';
-# 
-# #Cinder
-# CREATE DATABASE cinder;
-# GRANT ALL ON cinder.* TO 'cinderUser'@'%' IDENTIFIED BY 'cinderPass';
-# 
-# quit;
-
-
+  
 #
 # PoC para la creación de las bases de datos
 #
@@ -38,6 +13,16 @@ CREATE DATABASE ${DB};
 GRANT ALL ON ${DB}.* TO '${USER}'@'%' IDENTIFIED BY '${PASSWORD}';
 __QUERY__
 }
+
+#
+# ~/.temporal_pass tiene que estar creado y es de la forma:
+# - mysql:
+#   user: root
+#   password: $(openssl rand -hex 16)
+# - rabbitmq:
+#   user: openstack
+#   password: $(openssl rand -hex 16)
+#
 
 ROOTPASS=$(grep -A2 mysql ~/.temporal_pass | grep password | awk '{print $NF}')
 QUERY=$(date +%s).${0##*/}.sql
