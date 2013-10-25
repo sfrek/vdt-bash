7a00212d89820b1e46132d8cf2e9ab949b3b4d4b
Fernando Israel García Martínez <frekofefe@gmail.com>
2013-10-25 14:29:41 +0200

    More keystone actions

--- keystone.sh
+++ keystone.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 # We need:
-# export OS_SERVICE_TOKEN=TheAdminToken
-# export OS_SERVICE_ENDPOINT=http://controller:35357/v2.0
+export OS_SERVICE_TOKEN=TheAdminToken
+export OS_SERVICE_ENDPOINT=http://controller:35357/v2.0
+
 function debug(){
 	echo -e "\033[01;37m[debug]\033[00m ${@}"
 }
 
-function get_id (){
-    echo `$@ | awk '/ id / { print $4 }'`
+
+function get_info(){
+	local ACTION=${1}-list
+	keystone ${ACTION}
+}
+
+function get_id(){
+	local ACTION=${1}-list
+	local PATTERN=${2}
+	keystone ${ACTION} | awk '/'${PATTERN}'/ {print $2}'
+}
+
+function get_service_id(){
+	keystone service-list | awk '/'${1}'/ {print $2}'
 }
 
 function create_tenant(){
@@ -41,17 +54,19 @@ function create_admin(){
 	keystone user-role-add --user=admin --tenant=admin --role=admin
 }
 
+debug "keyston service id: " $(get_service_id keystone)
+debug "keyston service id: " $(get_id service keystone)
+debug "user admin id: " $(get_id user admin)
+debug "tenant admin id: " $(get_id service)
+get_info tenant
 
 # admin #
-create_admin Labo.!2013
-create_tenant service Service Tenant
+# create_admin Labo.!2013
+# create_tenant service Service Tenant
 
 # FIXME:
 # Make yml or json with initial keystone definitions...
 
 # identity service and end point
-END_POINT_ID=$(get_id keystone service-create --name=keystone --type=identity --description="Keystone Identity Service")
-keystone endpoint-create --service-id=${END_POINT_ID} \
-	--publicurl=http://controller:5000/v2.0 \
-	--internalurl=http://controller:5000/v2.0 \
-	--adminurl=http://controller:35357/v2.0
+# END_POINT_ID=$(get_id keystone service-create --name=keystone --type=identity --description="Keystone Identity Service")
+# keystone endpoint-create --service-id=${END_POINT_ID} --publicurl=http://controller:5000/v2.0 --internalurl=http://controller:5000/v2.0 --adminurl=http://controller:35357/v2.0
